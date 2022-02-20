db7f622528d21d07e3c13f3681611a6156dd3d8f
Anshul-99 <anshulsomani.99@gmail.com>
2022-02-20 01:09:10 -0700

    Assignment5_1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -3,21 +3,23 @@
 #include <netdb.h>
 #include "errno.h"
 #include <syslog.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 //#define DOMAIN_SOCKET (PF_INET6)
 #define SERVICE_PORT "9000"
 #define PATH_TO_FILE "/var/tmp/aesdsocketdata.txt"
-#define BYTES 512
+#define BYTES 1024
 
 int main()
 {
-
-	//TODO: /*makefile */
 	
 	/*Start syslog daemon*/
-    openlog("writer", LOG_USER, LOG_DEBUG|LOG_ERR); 
+    openlog("aesdsocket", LOG_USER, LOG_DEBUG|LOG_ERR); 
 	
 
 	int errnum =0;
@@ -26,22 +28,15 @@ int main()
 	int ret_val;
 	const char *service_port = SERVICE_PORT;
 	
-	// Get socket fd
-	socket_fd = socket(PF_INET6, SOCK_STREAM, 0);
-	if(socket_fd == -1)
-	{
-		errnum = errno;
-		syslog(LOG_ERR, "Unable to create the socket. The error was %s\n", strerror(errno));
-		
-		printf("Error: Unable to create socket %d\n\r", errnum);
-		return -1;
-	}
+	uint32_t total_message_len = 0;
+
 	
 	//Create sockaddr
 	//struct sockaddr sock_addr;
 	struct addrinfo hints;
 	struct addrinfo *servinfo;
-	struct addrinfo *servinfo_connectingaddr;
+	struct sockaddr_storage servinfo_connectingaddr; //to store connecting address from accept()
+	socklen_t addr_size;
 	
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_flags = AI_PASSIVE;
@@ -58,151 +53,205 @@ int main()
 		return -1;
 	}
 	
-	servinfo->sa_family = AF_INET6
-	//TODO:
-	servinfo->sa_data; //go through beej guide section 3.3
-	
-	ret_val = getaddrinfo(NULL, service_port, &hints, &servinfo_connectingaddr);
-	if(ret_val != 0)
+	// Get socket fd
+	socket_fd = socket(servinfo->ai_family, servinfo->ai_socktype, 0);
+	if(socket_fd == -1)
 	{
 		errnum = errno;
-		syslog(LOG_ERR, "getaddrinfo() returned error. The error was %s\n", strerror(errno));
+		syslog(LOG_ERR, "Unable to create the socket. The error was %s\n", strerror(errno));
 		
-		printf("Error: getaddrinfo() returned error %d\n\r", errnum);
+		printf("Error: Unable to create socket %d\n\r", errnum);
 		return -1;
 	}
 	
 	// bind()
-	ret_val = bind(socket_fd, servinfo, sizeof(struct sockaddr));
+	ret_val = bind(socket_fd, (servinfo->ai_addr), servinfo->ai_addrlen);
 	if(ret_val == -1)
 	{
 		errnum = errno;
 		syslog(LOG_ERR, "bind() returned error. The error was %s\n", strerror(errno));
 		
 		printf("Error: bind() returned error %d\n\r", errnum);
-	}
-	
-	//TODO: while() check if SIGINT or SIGTERM is received
-	
-	//listen()
-	ret_val = listen(sock_fd, 10); /* backlog value might cause problems */
-	if(ret_val == -1)
-	{
-		errnum = errno;
-		syslog(LOG_ERR, "listen() returned error. The error was %s\n", strerror(errno));
-		
-		printf("Error: listen() returned error %d\n\r", errnum);
-	}
-	
-	
-	//accept()
-	socket_fd_connected = accept(sock_fd, servinfo_connecting_addr, sizeof(struct sockaddr));
-	if(socket_fd_connected == -1)
-	{
-		errnum = errno;
-		syslog(LOG_ERR, "accept() returned error. The error was %s\n", strerror(errno));
-		
-		printf("Error: accept() returned error %d\n\r", errnum);
-	}	
-	
-	//read/write using recv()/send()
-
-	//TODO:
-	char* incoming_data = (char *)malloc(BYTES*sizeof(char)); /* realloc if buffer is full */
-	/* Read , write and re-read again. */
-	/* read byte by byte */
-	if(incoming_data == NULL)
-	{
-		printf("Unable to malloc\n\r");
+		remove("/var/tmp/aesdsocketdata.txt");
 		return -1;
 	}
 	
-	ssize_t msg_length = 0;
-	uint32_t index_buffer = 0;
-	uint8_t rx_complete =0;
-	int fd;
-	uint32_t total_message_len = 0;
+	int fd = open(PATH_TO_FILE, O_RDWR | O_CREAT | O_APPEND, 0644);
 	
-	while(1)
+	//TODO: while() check if SIGINT or SIGTERM is received
+
+	int z =3;
+	while(z--)
 	{
-		msg_length = recv(socket_fd_connected, incoming_data, BYTES, MSG_DONTWAIT);
-		if(msg_length <=0)
+		
+		//listen()
+		ret_val = listen(socket_fd, 10); /* backlog value might cause problems */
+		if(ret_val == -1)
 		{
-			errnum = errno; //error logging
+			errnum = errno;
+			syslog(LOG_ERR, "listen() returned error. The error was %s\n", strerror(errno));
 			
-			syslog(LOG_ERR, "recv() returned error. The error was %s\n", strerror(errno));
-			/* No more messages available */
-			printf("No more data is available to read. recv() returned error %d\n\r", errnum);
-			//write to file 
-			
-			rx_complete = 1; //set complete flag
+			printf("Error: listen() returned error %d\n\r", errnum);
 		}
-		else
+		
+		addr_size = sizeof servinfo_connectingaddr;
+		//accept()
+		socket_fd_connected = accept(socket_fd, (struct sockaddr *)&servinfo_connectingaddr, &addr_size);
+		if(socket_fd_connected == -1)
 		{
-			total_message_len += msg_length;
+			errnum = errno;
+			syslog(LOG_ERR, "accept() returned error. The error was %s\n", strerror(errno));
+			
+			printf("Error: accept() returned error %d\n\r", errnum);
 		}
+		
+		//read/write using recv()/send()
 	
-		//  write into the file byte by byte
-		while(index_buffer<msg_length) 
+		
+		
+		char* incoming_data = (char *)malloc(BYTES*sizeof(char)); /* realloc if buffer is full */
+		/* Read , write and re-read again. */
+		/* read byte by byte */
+		if(incoming_data == NULL)
 		{
-			if(incoming_data[index_buffer] != '\n') //accordingly write/append to the file
-			{
-				fd = open(PATH_TO_FILE, O_RDWR | O_CREAT /*| O_TRUNC*/, 0644);
-				//write the byte into the file
-				ret_val = write(fd, &incoming_data[index_buffer], 1);
-				if(ret_val == -1)
-				{
-					errnum = errno; //error logging			
-					syslog(LOG_ERR, "write() returned error. The error was %s\n", strerror(errno));
-					
-					printf("write() returned error %d\n\r", errnum);
-				}
-				
-			}
-			else if(incoming_data[index_buffer] == '\n')
-			{
-				fd = open(PATH_TO_FILE, O_RDWR | O_CREAT |O_APPEND/*| O_TRUNC*/, 0644); //append the byte into the file
-				
-				//add new line into the file to separate the packets
-				write(fd, &incoming_data[index_buffer], 1);
-				if(ret_val == -1)
-				{
-					errnum = errno; //error logging			
-					syslog(LOG_ERR, "write() (append) returned error. The error was %s\n", strerror(errno));
-					
-					printf("write() (append) returned error %d\n\r", errnum);
-				}
-			}
-			index_buffer++;
+			printf("Unable to malloc\n\r");
+			return -1;
 		}
-		close(fd);
+		
+		ssize_t msg_length = 0;
+		
+		uint8_t rx_complete =0;
+		
+		
+		while(1)
+		{
+			uint32_t index_buffer = 0;
+			msg_length = recv(socket_fd_connected, incoming_data, BYTES, MSG_DONTWAIT);
+			if(msg_length == -1)
+			{
+				errnum = errno; //error logging
+				
+				
+				if(errnum == 11)
+				{
+					printf("Recv() complete\n\r");
+					//continue;
+					break;
+				}
+				
+				syslog(LOG_ERR, "recv() returned error. The error was %s\n", strerror(errno));
+				/* No more messages available */
+				printf("No more data is available to read. recv() returned error %d\n\r", errnum);
+				//write to file 
+				rx_complete = 1; //set complete flag
+				
+			}
+			else
+			{
+				total_message_len += msg_length;
+			}
+			
+					//check if complete flag is set and exit loop
+			if(rx_complete)
+			{
+				break;
+			}
+		
+			//  write into the file byte by byte
+			while(index_buffer<msg_length) 
+			{
+				if(incoming_data[index_buffer] != '\n') //accordingly write/append to the file
+				{
+					//write the byte into the file
+					ret_val = write(fd, &incoming_data[index_buffer], 1);
+					if(ret_val == -1)
+					{
+						errnum = errno; //error logging			
+						syslog(LOG_ERR, "write() returned error. The error was %s\n", strerror(errno));
+						
+						printf("write() returned error %d\n\r", errnum);
+					}
+					printf("W: %s\n\r", &incoming_data[index_buffer]);
+					
+				}
+				else if(incoming_data[index_buffer] == '\n')
+				{
+					
+					
+					//add new line into the file to separate the packets
+					write(fd, &incoming_data[index_buffer], 1);
+					if(ret_val == -1)
+					{
+						errnum = errno; //error logging			
+						syslog(LOG_ERR, "write() (append) returned error. The error was %s\n", strerror(errno));
+						
+						printf("write() (append) returned error %d\n\r", errnum);
+					}
+					printf("W_A: %s\n\r", &incoming_data[index_buffer]);
+				}
+				index_buffer++;
+			}
+			/*close(fd);
+			if(ret_val == -1)
+			{
+				errnum = errno; //error logging			
+				syslog(LOG_ERR, "close() returned error. The error was %s\n", strerror(errno));
+				
+				printf("close() returned error %d\n\r", errnum);
+			}*/
+		}
+		
+		char *outgoing_data = (char *)(malloc(total_message_len*sizeof(char)));
+		
+		//fd = open(PATH_TO_FILE, O_RDWR, 0644);
+		lseek(fd, 0, SEEK_SET);
+		ret_val = read(fd, outgoing_data, total_message_len);
 		if(ret_val == -1)
 		{
 			errnum = errno; //error logging			
-			syslog(LOG_ERR, "close() returned error. The error was %s\n", strerror(errno));
+			syslog(LOG_ERR, "read() returned error. The error was %s\n", strerror(errno));
 			
-			printf("close() returned error %d\n\r", errnum);
+			printf("read() returned error %d\n\r", errnum);
 		}
-
-
-		//check if complete flag is set and exit loop
-		if(rx_complete)
+		printf("retval: %d\n\r", ret_val);
+		printf("T: %d\n\r", total_message_len);
+		printf("R: %s", outgoing_data);
+		
+		//send()
+		ret_val = send(socket_fd_connected, outgoing_data, total_message_len, MSG_DONTWAIT);
+		if(ret_val == -1)
 		{
-			break;
+			errnum = errno; //error logging			
+			syslog(LOG_ERR, "send() returned error. The error was %s\n", strerror(errno));
+			
+			printf("send() returned error %d\n\r", errnum);
 		}
+		printf("retval: %d\n\r", ret_val);
+		
+		
+		free(incoming_data);
+		free(outgoing_data);
+		close(socket_fd_connected);
+		
+		
 	}
 	
-	uint8_t * outgoing_data = (char *)malloc(total_message_len*sizeof(char));
+	close(fd);
+	if(ret_val == -1)
+	{
+		errnum = errno; //error logging			
+		syslog(LOG_ERR, "close() returned error. The error was %s\n", strerror(errno));
+		
+		printf("close() returned error %d\n\r", errnum);
+	}
+	freeaddrinfo(servinfo);
+	remove("/var/tmp/aesdsocketdata.txt");
 	
-	fd = open(PATH_TO_FILE, O_RDWR | O_CREAT /*| O_TRUNC*/, 0644);
-	read(fd, outgoing_data, total_message_len);
+	close(socket_fd);
+}		
 	
-	//send()
-	send(socket_fd_connected, outgoing_data, total_message_len, MSG_DONTWAIT);
+
+
 	
-	free(incoming_data);
-	free(outgoing_data);
-	
-}
 
 
