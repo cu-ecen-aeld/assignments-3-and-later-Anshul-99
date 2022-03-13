8f08bcb5a687e399edc40a254e5200d3b68c70d8
Anshul-99 <anshulsomani.99@gmail.com>
2022-03-13 13:34:12 -0600

    A8 aesdchar testing

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -266,6 +266,12 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
 	 	}
 	 	
 	 	PDEBUG("Write to circular buffer %s", dev->temp_write_buffer.buffptr);
+	 	
+	 	int i;
+	 	for(i =0; i< AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; i++)
+	 	{
+	 		PDEBUG("circular buffer content %d %s", i, dev->circ_buff[i].buffptr);
+	 	}
 	 	dev->temp_write_buffer.size =0;
 	 	dev->temp_write_buffer.buffptr = NULL;
 	 }
